0a94b5f73f4a05c70a14b2d378a59e8452105d01
hemadevi-nyros <40348452+hemadevi-nyros@users.noreply.github.com>
2018-12-08 09:04:28 +0530

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,23 +1,18 @@
 class Article < ApplicationRecord
-	belongs_to :category
-	belongs_to :author
+  belongs_to :category
+  belongs_to :author
   has_many  :languages
-	
-	after_destroy :log_destroy_action
+  after_destroy :log_destroy_action
   def log_destroy_action
     puts 'Article destroyed'
   end
-
   after_create :article_publishing 
   private
   def article_publishing
     puts "article is published"
-    
     self.status = "success"
-
     self.save!
   end
-  
   validates :name, presence: true, length: { minimum: 6}, uniqueness: true
   validates :description, presence: true, length: { minimum: 15}
   validates :is_published, acceptance: true
